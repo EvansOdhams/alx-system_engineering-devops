ab58d18f474e6b067e9048acc6209df0c4aef08e
EvansOdhams <evansodhams@gmail.com>
2023-10-10 20:09:35 -0700

    changes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None.
+2-recurse
 """
-
 import requests
 
-
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=[], after=None):
     """
-    Queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit.
-
-    - If not a valid subreddit, return None.
+    Recursively query the Reddit API and return a list of hot article titles.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"after": after},
-    )
+    base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            hot_list.append(title)
-        after = req.json().get("data").get("after")
+    params = {'limit': 100, 'after': after}
+    response = requests.get(base_url, headers=headers, params=params, allow_redirects=False)
 
-        if after is None:
-            return hot_list
-        else:
-            return recurse(subreddit, hot_list, after)
-    else:
+    if response.status_code != 200:
         return None
+
+    data = response.json().get('data', {})
+    children = data.get('children', [])
+    for child in children:
+        hot_list.append(child['data']['title'])
+
+    after = data.get('after')
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
+
+# Example usage
+if __name__ == '__main__':
+    subreddit = "programming"
+    result = recurse(subreddit)
+    if result is not None:
+        print(len(result))
+    else:
+        print("None")
